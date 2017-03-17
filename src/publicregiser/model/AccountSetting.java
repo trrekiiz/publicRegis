08902d15b3d78b3dbf995c7cd69b2531e8cd00c3
newsonesnsd <newsonesnsd@gmail.com>
2017-03-17 19:20:37 +0700

    move AccountSetting from view to Package

--- src/publicregiser/model/AccountSetting.java
+++ src/publicregiser/model/AccountSetting.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package publicregiser.view;
+package publicregiser.model;
 
 import java.awt.Toolkit;
 import sun.awt.image.ToolkitImage;
@@ -41,7 +41,6 @@ public class AccountSetting extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Account Setting");
-        setMaximumSize(new java.awt.Dimension(1024, 768));
         setMinimumSize(new java.awt.Dimension(1024, 768));
         setResizable(false);
 
